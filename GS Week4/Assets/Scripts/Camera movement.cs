b98a7f495ab7383d4ec6423eb875fc9758e42e2b
Hastur0451 <zy20020220@gmail.com>
2024-03-16 21:56:29 +1100

    Add camera control

---
+++ GS Week4/Assets/Scripts/Camera movement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameramovement : MonoBehaviour
+{
+    public GameObject followObject;
+    public Vector2 followOffset;
+    public float speed = 3f;
+    private Vector2 threshold;
+    private Rigidbody2D rb;
+
+    private float lastXPosition;
+
+    void Start()
+    {
+        threshold = calculateThreshold();
+        rb = followObject.GetComponent<Rigidbody2D>();
+        lastXPosition = transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 follow = followObject.transform.position;
+        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+
+        Vector3 newPosition = transform.position;
+        if (Mathf.Abs(xDifference) >= threshold.x)
+        {
+            newPosition.x = follow.x > lastXPosition ? follow.x : lastXPosition;
+        }
+
+        float followYThreshold = 8.4f;
+        if (Mathf.Abs(yDifference) >= threshold.y && follow.y > followYThreshold)
+        {
+            newPosition.y = follow.y;
+        }
+
+        float minYPosition = 0f;
+        newPosition.y = Mathf.Max(newPosition.y, minYPosition);
+
+        float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+
+        lastXPosition = transform.position.x;
+    }
+    private Vector3 calculateThreshold()
+    {
+        Rect aspect = Camera.main.pixelRect;
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        t.x -= followOffset.x;
+        t.y -= followOffset.y;
+        return t;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 border = calculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    }
+}
